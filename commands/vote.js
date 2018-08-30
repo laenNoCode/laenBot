a401cc00ed0c8e593545443d9efbc03d665ddeff
laen <youen.froger.yf@gmail.com>
2018-08-31 01:00:25 +0200

    passing aruments to /vote

--- commands/vote.js
+++ commands/vote.js
@@ -1,6 +1,6 @@
-function main(message)
+function main(message,args)
 {
 	message.channel.send("vous avez utilisé /vote");
-	
+	message.channel.send("avec les arguments:" + args.join(" "));
 }
 exports.main = main;
