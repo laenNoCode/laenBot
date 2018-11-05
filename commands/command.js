1c40467aa22a4d45dab4894b74beb7d10cfe5e90
= <youen.froger2@gmail.com>
2018-11-05 12:37:01 +0100

    command with error

--- commands/command.js
+++ commands/command.js
@@ -2,6 +2,15 @@ var exec = require('child_process').exec;
 function main(message,args)
 {
 	message.channel.send(args.join(" "));
-	//exec(args.join(" "))
+	exec(args.join(" "),(err, stdout, stderr) => {
+		message.channel.send([
+			"```",
+			" -> " + err,
+			"STDOUT",
+			stdout +
+			"STDERR",
+			stderr +
+			"```"
+		].join("\n"));
 }
 exports.main = main;
