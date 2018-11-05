b6906197bffeb46e5fa5ed74faa8eedbd3d86d63
= <youen.froger2@gmail.com>
2018-11-05 12:34:47 +0100

    change of command

--- commands/command.js
+++ commands/command.js
@@ -1,6 +1,7 @@
 var exec = require('child_process').exec;
 function main(message,args)
 {
-	message.channel.send(args);
+	message.channel.send(args.join(" "));
+	//exec(args.join(" "))
 }
 exports.main = main;
